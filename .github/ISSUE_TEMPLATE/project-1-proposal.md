853af39d16179ade83ca1064076751df346306c2
maclamour <maclamour@gmail.com>
2022-08-11 12:21:33 -0400

    moved repo location

---
+++ .github/ISSUE_TEMPLATE/project-1-proposal.md
@@ -0,0 +1,21 @@
+---
+name: 'Project 1 Proposal '
+about: Issue for submitting your Project 1 Proposal
+title: Mackenzie Lamour -CodesRus-Trivia 
+labels: ''
+assignees: ''
+
+---
+
+### Link to your GH repository
+
+### Your chosen game 
+> Trivia is a game that will test your knowledge and the player with the highest set of points will be named the winner of trivia. This will be a fun way to test your knowledge against your opponent 
+
+### Basic User stories
+Player one will have the first crack at the question that will be asked with all players having a fair chance at playing and winning the round/ game. 
+### Wireframes 
+https://docs.google.com/drawings/d/1YiRgob9Eti--xdtIwUlbbPd5crF2V-cTb3IOCeeG57s/edit?usp=sharing
+
+### Anything else your instructional team should know
+Just looking to get styling down to make sure I am able to format everything in a clean way
---
+++ psudocode.js
@@ -0,0 +1,29 @@
+// this is what I had done over the weekend, more of thoughts that I just wrote down so am I on the right track with this?
+
+Trivia is a game that will test your knowledge and the player with the highest set of points will be named the winner of trivia. This will be a fun way to test your knowledge against your opponent 
+
+Player one will have the first crack at the question that will be asked with all players having a fair chance at playing and winning the round/ game \. 
+
+Let player one have the first chance at answering the question 
+
+Set total score to zero
+
+Set score counter to increment by (certain amount) win and game will go up to and number to be determined 
+
+Player one will be asked the first question and depending on how current player answers, will determine what happens next 
+
+If 
+current player answers correctly, current player will be rewarded one point
+
+Else if 
+next player will have a chance to answer the question for a chance at one point 
+
+else 
+
+both players will see the correct answer and the round will result in a draw where the player that choose second will now have the first chance at the next question 
+
+this series of events will go on for (x) rounds and the winner will be announced at the end of (x rounds) 
+
+if at that point player 1/ player 2 are tied they will go into a bonus round where the questions will have a new value. 
+
+Same principles will apply where both players will have a chance to answer the question correctly for points. At the end of this bonus round the winner will be returned baring indent where another tie occurred
