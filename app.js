f7eba3fc4aedbd842753ac513e52214d231b42bf
maclamour <maclamour@gmail.com>
2022-08-14 23:42:26 -0400

    trying to isolate correct answer from question bank

--- app.js
+++ app.js
@@ -1,50 +1,17 @@
-// const timerDisplay =document.querySelector('#timer')
-// const startBtn = document.querySelector('button')
-// let intervel
 
-// let ticks = 0
-
-// startBtn.addEventListener("click", function(){
-//     console.log('testing button')
-//     // clearInterval(intervel)
-//     let intervel = setInterval(timerFunction, 1000)
-//     // console.log(intervel)
-// })
-
-// function timerFunction(){
-// console.log("sei rocks")
-// ticks++
-// console.log(ticks)
-// }
-
-// document.querySelector("form").reset();
-
-// const resetButton = document.querySelector(".resetbotton")
-
-// need to set event that is able to call on the questions class and show questions that aew in my array
-// question.forEach(question=> {
-// question.addEventListener('click',)
-// });
-
-// let playerOne= "Bob"
 
 let currentQuestion = 0
-const startButton = document.querySelector("#start-btn")
-const nextButton = document.querySelector("#next-btn")
-const questionContanierElement = document.querySelector("#question-contanier")
-const questionElement = document.querySelector("#question")
 
-const answerButtonElement = document.querySelector("#answer-buttons")
-
-
-// let choice1 = document.getElementById("choice1");
-// let choice2 = document.getElementById("choice2");
-// let choice3 = document.getElementById("choice3");
-// let choice4 = document.getElementById("choice4");
-// console.log(choice1);
-// console.log(start);
-// let correctAnswer = document.getElementsByClassName("correctAnswer")
-// console.log(correctAnswer)
+const questionList = document.getElementsByClassName("question");
+const choiceList = document.querySelectorAll(".btn");
+const start = document.getElementById("go");
+// let real
+let choice1 = document.getElementById("choice1");
+let choice2 = document.getElementById("choice2");
+let choice3 = document.getElementById("choice3");
+let choice4 = document.getElementById("choice4");
+let correctAnswer = document.getElementById("correctAnswer")
+console.log(correctAnswer)
 // console.log(choice1);
 // console.log(start);
 
@@ -52,144 +19,149 @@ const questionBank = [
   {
     question: "In what city and state was the game of basketball created?",
     answers: [
-        {text: "Homer, Alaska", correct:false},
-        {text: "Kingman, Azizona", correct:false},
-        {text: "Springfield, Massachusetts", correct:true},
-        {text: "Las Vegas, Nevada", correct:false},
-    ]
-       },
+      { text: "Homer, Alaska"},
+      { text: "Kingman, Azizona"},
+      { text: "Springfield, Massachusetts"},
+      { text: "Las Vegas, Nevada"},
+    ],
+    correct: "Springfield, Massachusetts"
 
-{ 
+  },
+
+  {
     question: "What is the capital of the USA?",
     answers: [
-        {text: "Washigton D.C.", correct: true},
-        {text: "Boulder", correct: false},
-        {text: "Bogota", correct: false},
-        {text: "Nashville", correct: false},
-      ]
+      { text: "Washigton D.C."},
+      { text: "Boulder"},
+      { text: "Bogota"},
+      { text: "Nashville"},
+    ],
+    correct: "Washigton D.C."
+  },
 
-    },
-
-{
+  {
     question: "Whos is Mickey Mouse's wife",
     answers: [
-        {text: "Ariel", correct: false},
-        {text: "Jamsmine", correct: false},
-        {text: "Belle", correct: false},
-        {text: "Miney Mouse", correct: true},
-
+      { text: "Ariel"},
+      { text: "Jamsmine"},
+      { text: "Belle"},
+      { text: "Miney Mouse"},
     ],
-    },
+    correct: "Miney Mouse"
+  },
 
-{
+  {
     question: "In a website browser address bar, what does www stand for?",
     answers: [
-        {text: "World Wide Winners", correct: false},
-        {text: "World Wide Wages", correct: false},
-        {text: "World Wide Web", correct: true},
-        {text: "World Wide Weather", correct: false},
-
+      { text: "World Wide Winners"},
+      { text: "World Wide Wages"},
+      { text: "World Wide Web"},
+      { text: "World Wide Weather"},
     ],
-
-    },
-{
+    correct: "World Wide Web"
+  },
+  {
     question: "What is the indeity of Superman?",
     answers: [
-        {text: "Peter Parker", correct: false},
-        {text: "Tom Brady", correct: false},
-        {text: "Yusuke Urameshi", correct:false},
-        {text: "Clark Kent", correct: true},
-
+      { text: "Peter Parker"},
+      { text: "Tom Brady"},
+      { text: "Yusuke Urameshi"},
+      { text: "Clark Kent"},
     ],
-
-    },
-{
+    correct: "Clark Kent"
+  },
+  {
     question: "In the MCU, who was the first Advenger?",
     answers: [
-
-        {text: "Capital America", correct: true},
-        {text: "Bruce Banner", correct: false},
-        {text: "Iron Man", correct:false},
-        {text: "Thor", correct: false},
-
+      { text: "Capital America"},
+      { text: "Bruce Banner"},
+      { text: "Iron Man"},
+      { text: "Thor"},
     ],
-},
-
+    correct: "Capital America"
+  },
 ];
 
-console.log(questionBank)
 
-
-let shuffleQuestions, currentQuestionIndex
-const startGame = () => {
-  startButton.classList.add("hide")
-  shuffleQuestions = questionBank.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0
-  questionContanierElement.classList.remove("hide");
-  setNextQuestion();
-  startButton.addEventListener("click", startGame)
-};
-
-// startButton.addEventListener("click", startGame)
-
-const setNextQuestion = () => {
-  resetState ();
-  displayQuestion(shuffleQuestions[currentQuestionIndex]);
-
-};
-
-nextButton.addEventListener("click", () => {
-  currentQuestionIndex++
-  setNextQuestion();
-});
-
-const displayQuestion = (question) => {
-  questionElement.innerText = question.question; 
-  question.answers.forEach((answer) => {
-    const button = document.createElement  ("button");
-    button.innerText = answer.text; 
-    button.classList.add("btn");
-    if (answer.correct){
-      button.dataset.correct = answer.correct; 
-    }
-    button.addEventListener("click", chooseAnswer);
-    answerButtonElement.appendChild(button);
-  });
-}; 
-
-
- 
 // gives random numbers- translates to index number and gives back a different question
 
-// for (let i = 0; i < questionBank.length; i++) {
-//   questionBank.sort(() => Math.random() - 0.5);
-//   // works but i find that this will give me the same questions alot of the time
-// }
-// for (let i = 0; i < questionBank.length; i++) {
-//   questionBank.sort(() => Math.random() - 0.5);
-//   // works but i find that this will give me the same questions alot of the time
-// }
-// // check to see if we are able to breakdown questionBank- answer- true/false
+for (let i = 0; i < questionBank.length; i++) {
+  questionBank.sort(() => Math.random() - 0.5);
+  // works but i find that this will give me the same questions alot of the time
+}
 
-// // start button
-//   choiceList.forEach((choiceList) => {
-//     let choice = e.target.innerText;
-//     console.log(choice);
-//     currentQuestion++
-//     winLogic()
-//     ready()
-//   });
+// console.log(questionBank[0].correct) // isolates the correct answer field 
+// console.log(choice1.innerHTML)
+// console.log(choiceList)
 
-// });
+// check to see if we are able to breakdown questionBank- answer- true/false
+
+// start button
+
+function ready() {
+  testQuestion = questionBank[currentQuestion];
+  if (testQuestion){
+    question = testQuestion.question;
+    questionList[0].textContent = question;
+  
+    choice1.innerHTML = testQuestion.answers[0].text;
+    choice2.innerHTML = testQuestion.answers[1].text;
+    choice3.innerHTML = testQuestion.answers[2].text;
+    choice4.innerHTML = testQuestion.answers[3].text;
+  }
+
+  else {
+    alert("game over!!!!!")
+  }
+  
+
+  // homework for later
+  // for(let i = 0; i < testQuestion.answers.length; i++){
+  //         console.log(testQuestion.answers[i])
+  //     }
+}
+start.addEventListener("click", ready);
+
+// console.log(questionBank[1].answers)// works
+// console.log(questionBank[2].answers)// works
+// console.log(questionBank)
+// console.log(questionBank[0].answers)
+
+// testQuestion = questionBank[0]
+// console.log(testQuestion)
+
+// question = testQuestion.question
+// console.log(question)
+
+// questionList[0].textContent = question
+
+// trying to get my buttons to work and have each button show the text of the answers and return if it is
+// it is the correct answer or not based on the questions object
+choiceList.forEach((choiceList) => {
+  choiceList.addEventListener("click", function (e) {
+    console.log(testQuestion);
+    let choice = e.target.innerText;
+    console.log(choice);
+    if (choice === questionBank.correct){
+        correctAnswer.innerHTML = "correct"
+    }
+    else {
+        correctAnswer.innerHTML = "Wrong Answer"
+    }
+console.log(correctAnswer)
+
+    currentQuestion++
+    // winLogic()
+    ready()
+  });
+
+});
+
+
 
 // if(currentQuestion.answers=== "true"){
 //     alert ("true")
-// } 
-
-
-// // if(currentQuestion.answers=== "true"){
-// //     alert ("true")
-// // }
+// }
 
 // function winLogic(){
 //     console.log(choiceList,"choiceList")
@@ -200,4 +172,4 @@ const displayQuestion = (question) => {
 //         correctAnswer.textContent = "Wrong Answer"
 //     }
 // }
-// 4
+
